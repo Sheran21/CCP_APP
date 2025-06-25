a682fe72c42bd1b25475f5d59540b638f097135f
Sheran21 <samarejanula@gmail.com>
2025-06-25 13:18:17 +0530

    Fix Vite build: moved index.html to root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  root: 'src', // 👈 Important to let Vite know where your actual code lives
-})
+  plugins: [react()]
+});
